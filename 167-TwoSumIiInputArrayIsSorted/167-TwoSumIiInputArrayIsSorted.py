b5e7d7753df0aabb926c9e4d8e968de23642a231
BlueWhale <166388498+LewisLim-USA@users.noreply.github.com>
2025-06-25 04:10:36 +0800

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.py
@@ -0,0 +1,14 @@
+# Last updated: 6/25/2025, 4:10:35 AM
+class Solution:
+    def twoSum(self, numbers, target):
+        i = 0                      # Start pointer at the beginning
+        j = len(numbers) - 1       # End pointer at the end
+
+        while i < j:               # Keep checking while pointers don't cross
+            total = numbers[i] + numbers[j]  # Add the two pointed values
+            if total == target:              # If they match the target
+                return [i + 1, j + 1]         # Return 1-indexed positions
+            if total < target:               # If sum is too small
+                i += 1                        # Move start pointer right
+            else:                             # If sum is too big
+                j -= 1                        # Move end pointer left
