63178dbca2d142117e3cc05238fa7521fce3a1e1
BlueWhale <166388498+LewisLim-USA@users.noreply.github.com>
2025-06-25 04:10:24 +0800

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,20 @@
+# Last updated: 6/25/2025, 4:10:24 AM
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        
+        return result
+
+        
