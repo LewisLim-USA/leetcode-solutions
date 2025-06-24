62ed02381faef539be0b01f809d2cc464024ede1
BlueWhale <166388498+LewisLim-USA@users.noreply.github.com>
2025-06-25 04:12:09 +0800

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 6/25/2025, 4:12:08 AM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
