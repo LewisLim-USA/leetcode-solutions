2f73853ee5677fb586ba7302979a6b973e8273ad
BlueWhale <166388498+LewisLim-USA@users.noreply.github.com>
2025-06-25 04:10:40 +0800

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/25/2025, 4:10:40 AM
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
